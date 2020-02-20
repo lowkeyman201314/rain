13afc3cddefc0b4043f56d78d1143b362c3b1898
laoyu <480337728@qq.com>
2020-02-20 08:54:44 +0800

    实现首页展示雨量信息 调整首页信息倒序排列 配置web.xml，实现打开首页自动显示所有雨量信息 构建war包

--- src/dao/RainManageDao.java
+++ src/dao/RainManageDao.java
@@ -28,7 +28,7 @@ public class RainManageDao extends BaseDao {
         RainQuality rainQuality = null;
         try {
             //编写SQL语句
-            String sql = "SELECT id,districtName,monitorTime,rain,monitoringStation,monitoringAddress FROM `rainquality`";
+            String sql = "SELECT id,districtName,monitorTime,rain,monitoringStation,monitoringAddress FROM `rainquality` ORDER  BY monitorTime DESC ";
             //获取数据库连接
             conn = BaseDao.getConn();
             //创建执行SQL的对象
